382075d81e2b31ff3c675974ab578cfee3c35081
Aaron Vontell <vontell@mit.edu>
2016-05-28 14:31:07 -0400

    Removed authentication; solution in works

--- app.js
+++ app.js
@@ -2,137 +2,23 @@
 // ---- CONFIGURATION ---------------------------------------------------------
 // ----------------------------------------------------------------------------
 
-var express = require('express'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    session = require('express-session'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local');
-
-var config = require('./config.js'), //config file contains all tokens and other private info
-    funct = require('./functions.js'); //funct file contains our helper functions for our Passport and database work
-
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// Configure Express
-app.use(logger('combined'));
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Session-persisted message middleware
-app.use(function(req, res, next){
-    var err = req.session.error,
-        msg = req.session.notice,
-        success = req.session.success;
-
-    delete req.session.error;
-    delete req.session.success;
-    delete req.session.notice;
-
-    if (err) res.locals.error = err;
-    if (msg) res.locals.notice = msg;
-    if (success) res.locals.success = success;
-
-    next();
-});
-
 // ----------------------------------------------------------------------------
-// ---- PASSPORT --------------------------------------------------------------
+// ---- AUTHENTICATION --------------------------------------------------------
 // ----------------------------------------------------------------------------
-
-// Use the LocalStrategy within Passport to login/”signin” users.
-passport.use('local-signin', new LocalStrategy(
-  {passReqToCallback : true}, //allows us to pass back the request to the callback
-  function(req, username, password, done) {
-    funct.localAuth(username, password)
-    .then(function (user) {
-      if (user) {
-        console.log("LOGGED IN AS: " + user.username);
-        req.session.success = 'You are successfully logged in ' + user.username + '!';
-        done(null, user);
-      }
-      if (!user) {
-        console.log("COULD NOT LOG IN");
-        req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
-        done(null, user);
-      }
-    })
-    .fail(function (err){
-      console.log(err.body);
-    });
-  }
-));
-// Use the LocalStrategy within Passport to register/"signup" users.
-passport.use('local-signup', new LocalStrategy(
-  {passReqToCallback : true}, //allows us to pass back the request to the callback
-  function(req, username, password, done) {
-    funct.localReg(username, password)
-    .then(function (user) {
-      if (user) {
-        console.log("REGISTERED: " + user.username);
-        req.session.success = 'You are successfully registered and logged in ' + user.username + '!';
-        done(null, user);
-      }
-      if (!user) {
-        console.log("COULD NOT REGISTER");
-        req.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
-        done(null, user);
-      }
-    })
-    .fail(function (err){
-      console.log(err.body);
-    });
-  }
-));
-
-// Passport session setup.
-passport.serializeUser(function(user, done) {
-  console.log("serializing " + user.username);
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  console.log("deserializing " + obj);
-  done(null, obj);
-});
-
-// Simple route middleware to ensure user is authenticated.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  else {console.log("User not authenticated");}
-  req.session.error = 'Please sign in!';
-  res.redirect('/signin');
-}
 
 // ----------------------------------------------------------------------------
 // ---- ROUTING ---------------------------------------------------------------
 // ----------------------------------------------------------------------------
 
-app.get('/', ensureAuthenticated, function(request, response) {
+app.get('/', function(request, response) {
   response.send('Hello world!');
-});
-
-// Attempts to authenticate the user
-app.post('/signup', passport.authenticate('local-signup'), function(req, res) {
-    response.send(req);
-});
-
-app.get('/logout', function(req, res){
-    var name = req.user.username;
-    console.log("LOGGIN OUT " + req.user.username)
-    req.logout();
-    req.session.notice = "You have successfully been logged out " + name + "!";
 });
 
 // ----------------------------------------------------------------------------
