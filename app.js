e748a95edae9248956509b3614cb17f8ff2fbb02
Aaron Vontell <vontell@mit.edu>
2016-05-29 12:59:44 -0400

    Channels now creatable on backend

--- app.js
+++ app.js
@@ -2,12 +2,36 @@
 // ---- CONFIGURATION ---------------------------------------------------------
 // ----------------------------------------------------------------------------
 
-var express = require('express');
+var express = require('express'),
+    mongo   = require('mongodb'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+var mongoUri = 'mongodb://heroku_wx2l3rwk:k8qsr29notgbfnqcpkqqtuuvvp@ds019033.mlab.com:19033/heroku_wx2l3rwk';
+mongoose.connect(mongoUri);
+
+// Define models of the database
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var Channel = new Schema({
+    title     : String,
+    history   : [{activated : Date}],
+    count     : Number,
+    created   : {username : String, timestamp : Date}
+});
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // ----------------------------------------------------------------------------
 // ---- AUTHENTICATION --------------------------------------------------------
@@ -18,7 +42,45 @@ app.use(express.static(__dirname + '/public'));
 // ----------------------------------------------------------------------------
 
 app.get('/', function(request, response) {
-  response.send('Hello world!');
+    response.send('Hello world!');
+});
+
+app.put('/channel', function(req, res) {
+    
+    // Make sure that each required field is present
+    // i.e. title
+    if(!req.body.title) {
+        res.status(400).send({success: false, err: "A title must be indicated"});
+        return;
+    }
+    
+    // Make sure that this title was not used previously
+    var channelModel = mongoose.model('Channel', Channel);
+    channelModel.find({title: req.body.title}, function (err, docs) {
+        if(docs.length > 0) {
+            res.status(400).send({success: false, err: "That channel already exists!"});
+            return;
+        } else {
+            
+            // If not, add the channel! TODO: Add user and time of creation
+            
+            var instance = new channelModel();
+            instance.title = req.body.title;
+            instance.save(function (err) {
+                res.send({id: instance._id, success : !err, err: err});
+            });
+        }
+    });
+    
+});
+
+app.get('/channel', function(req, res) {
+    
+    var channelModel = mongoose.model('Channel', Channel);
+    channelModel.find({}, function (err, docs) {
+        res.send(docs);
+    });
+    
 });
 
 // ----------------------------------------------------------------------------
--- readme.md
+++ readme.md
@@ -16,6 +16,33 @@ See the preceding section to see more information of the format of each object a
 
 #### Channels Creation
 
+In order to create a channel, an authenticated user will send a `PUT` request with the following information:
+
+```javascript
+{
+	"title": (String)
+}
+```
+
+If the request is valid with no errors, a JSON object will be sent back with relevant information, including an id that can be used to access the channel later:
+
+```javascript
+{
+  "id": (String, Unique),
+  "success": true,
+  "err": null
+}
+```
+
+If a request is made with a channel title that already exists, or without a title parameter, than an object with the error will be returned.
+
+```javascript
+{
+  "success": false,
+  "err": "That channel already exists!" | "A title must be indicated"
+}
+```
+
 #### Channel Subscriptions
 
 #### Channel Unsubscribing
@@ -28,10 +55,10 @@ Each channel will follow this format:
 
 ```javascript
 {
- 	id: (String, Unique),
-	title: (String),
-	history: [DateTime],
-	count: (Integer),
-	created: {DateTime, Username}
+ 	"id": (String, Unique),
+	"title": (String),
+	"history": [DateTime],
+	"count": (Integer),
+	"created": {DateTime, Username}
 }
 ```
